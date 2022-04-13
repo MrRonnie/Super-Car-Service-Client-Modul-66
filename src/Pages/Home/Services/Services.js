3ad54094e00785c15b4c92f385c0b70e97b33157
MrRonnie <ronymaza55@gmail.com>
2022-04-13 23:05:31 +0600

    services json data file add to the project

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,13 +1,17 @@
-import React from "react";
-
-const services = [
-  { id: 1, name: "oil change", price: 100, description: "", img: "" },
-];
+import React, { useEffect, useState } from "react";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div>
-      <h2>Services:</h2>
+      <h2>Services: {services.length}</h2>
     </div>
   );
 };
