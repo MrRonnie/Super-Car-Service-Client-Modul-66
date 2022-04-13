36aa618c37e7f257858f8d979983a207062d9205
MrRonnie <ronymaza55@gmail.com>
2022-04-14 01:27:21 +0600

    Bootstrap responsive

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -6,8 +6,9 @@ import expert3 from "../../../images/experts/expert-3.jpg";
 import expert4 from "../../../images/experts/expert-4.jpg";
 import expert5 from "../../../images/experts/expert-5.jpg";
 import expert6 from "../../../images/experts/expert-6.png";
+import Expert from "../Expert/Expert";
 
-const expert = [
+const experts = [
   { id: 1, name: "Will Smith", img: expert1 },
   { id: 2, name: "Chris Rock", img: expert2 },
   { id: 3, name: "Dwayna Rock", img: expert3 },
@@ -18,8 +19,13 @@ const expert = [
 
 const Experts = () => {
   return (
-    <div>
-      <h2>Our Experts</h2>
+    <div className="container">
+      <h2 className="text-primary text-center">Our Experts</h2>
+      <div className="row">
+        {experts.map((expert) => (
+          <Expert key={expert.id} expert={expert}></Expert>
+        ))}
+      </div>
     </div>
   );
 };
