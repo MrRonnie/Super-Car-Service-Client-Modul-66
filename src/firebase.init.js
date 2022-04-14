3c651f0a2f9831de6e4d9cd32de682e9801c47e2
MrRonnie <ronymaza55@gmail.com>
2022-04-14 19:49:05 +0600

    Email password based authentication with react firebase hooks

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA714Rc8nJsxU7r0bpnKmFdgv8EX9c63ig",
-  authDomain: "super-car-services.firebaseapp.com",
-  projectId: "super-car-services",
-  storageBucket: "super-car-services.appspot.com",
-  messagingSenderId: "551477338372",
-  appId: "1:551477338372:web:1a254c01cb0854365b3051",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
@@ -19,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-export default app;
+export default auth;
