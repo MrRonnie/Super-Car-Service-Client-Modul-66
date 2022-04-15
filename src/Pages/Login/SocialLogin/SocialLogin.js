08f09854fe18dfa4ad8731bd5664252a6a6da473
MrRonnie <ronymaza55@gmail.com>
2022-04-15 15:40:46 +0600

    Implement google login system and handle error

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,8 +2,26 @@ import React from "react";
 import google from "../../../images/social/google-logo.png";
 import facebook from "../../../images/social/facebook.png";
 import github from "../../../images/social/github.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorElement;
+  if (error) {
+    errorElement = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
+
   return (
     <div>
       <div className="d-flex align-items-center">
@@ -11,8 +29,12 @@ const SocialLogin = () => {
         <p className="mt-2 px-2">or</p>
         <div style={{ height: "1px" }} className="bg-dark w-50"></div>
       </div>
+      {errorElement}
       <div>
-        <button className="btn btn-light shadow-lg mb-2 w-50 d-block mx-auto">
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-light shadow-lg mb-2 w-50 d-block mx-auto"
+        >
           <img src={google} alt="" />
           <span className="px-2">Google Sign In</span>
         </button>
