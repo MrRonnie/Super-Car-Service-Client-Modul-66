f51f229bb4efb8eac41866bd202cba4bc9a0555d
MrRonnie <ronymaza55@gmail.com>
2022-04-25 12:58:02 +0600

    Insert Service data to the mongodb cloud database

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -8,21 +8,51 @@ const AddService = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    const url = `http://localhost:5000/service`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type="submit" />
-    </form>
+    <div className="w-50 mx-auto">
+      <h2>Please add a service</h2>
+      <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="mb-2"
+          placeholder="Name"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <textarea
+          className="mb-2"
+          placeholder="Description"
+          {...register("description")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Price"
+          type="number"
+          {...register("price")}
+        />
+        <input
+          className="mb-2"
+          placeholder="Photo URL"
+          type="text"
+          {...register("img")}
+        />
+        <input type="submit" value="Add Service" />
+      </form>
+    </div>
   );
 };
 
